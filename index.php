5c7d633d7e3b01ddd4887885252d9d4a0adeebe4
Unknown <kirby94@hotmail.it>
2018-05-19 22:21:11 +0200

    - Presentation Area: done

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+	<head>
+
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <?php require "php/common/headElements.php"; ?>
+        <link rel="stylesheet" href="css/index/desktop.css">
+
+		<title>Home</title>
+
+	</head>
+
+	<body class="text-center">
+
+		<div class="container-fluid">
+			
+
+
+                <div class="row presentationArea">
+
+                
+                    <!-- Name area -->
+                    <div class="col align-self-center">
+                        <div class="presentationAreaText">
+                            <h1 class="text-right">
+                                MARIO
+                                <br>
+                                MIGNEMI
+                            </h1>
+                            <h2 class="text-right">
+                                Bachelor's Degree in
+                                <br>
+                                Computer Science
+                            </h2>
+                        </div>
+                    </div>
+
+                    <!-- Photo area -->
+                    <div class="col">
+                        <img class="float-left" src="img/Mario.png">
+                    </div>
+
+                </div>
+
+
+		</div>
+
+		<?php require "php/common/jsForBootstrap.php"?>
+
+	</body>
+
+</html>
