954aa4a7e012f3767a29fda3716554c8628037b0
Unknown <kirby94@hotmail.it>
2018-05-19 23:29:37 +0200

    A brief story section

--- index.php
+++ index.php
@@ -18,57 +18,86 @@
 	<body class="text-center">
 
 		<div class="container-fluid">
-			
+            
+            <!-- Presentation Area -->
+            <div class="row presentationArea sectionGap">
 
+            
+                <!-- Name area -->
+                <div class="col align-self-center">
+                    <div class="presentationAreaText">
+                        <h1 class="text-right titleFontFamily">
+                            MARIO
+                            <br>
+                            MIGNEMI
+                        </h1>
+                        <h2 class="text-right subtitleFontFamily">
+                            Bachelor's Degree in
+                            <br>
+                            Computer Science
+                        </h2>
+                    </div>
+                </div>
 
-                <div class="row presentationArea">
+                <!-- Photo area -->
+                <div class="col">
+                    <img class="float-left" src="img/Mario.png">
+                </div>
 
-                
-                    <!-- Name area -->
-                    <div class="col align-self-center">
-                        <div class="presentationAreaText">
-                            <h1 class="text-right titleFontFamily">
-                                MARIO
-                                <br>
-                                MIGNEMI
-                            </h1>
-                            <h2 class="text-right subtitleFontFamily">
-                                Bachelor's Degree in
-                                <br>
-                                Computer Science
-                            </h2>
-                        </div>
+            </div>
+
+            <!-- About Me -->
+            <div class="row sectionGap">
+
+                <div class="col section text-center ash-grey">
+
+                    <div class="row mb-4">
+                        <h1 class="w-100 titleFontFamily font-italic font-weight-bold ">
+                            About me
+                        </h1>
                     </div>
 
-                    <!-- Photo area -->
-                    <div class="col">
-                        <img class="float-left" src="img/Mario.png">
+                    <div class="row">
+                        <p class="w-100 subtitleFontFamily">
+                            I am a recent graduate in Computer Science with a foreign languages diploma and some work experience whosedream is to become a full-time software developer.
+                        </p>
                     </div>
 
                 </div>
 
-                <!-- About Me -->
-                <div class="row">
+            </div>
 
-                    <div class="col section text-center ash-grey">
+            <!-- A brief story -->
+            <div class="row sectionGap">
+
+                <div class="col text-center ash-grey">
+
+                    <div class="circle">
 
                         <div class="row mb-4">
                             <h1 class="w-100 titleFontFamily font-italic font-weight-bold ">
-                                About me
+                                A brief story
                             </h1>
                         </div>
 
                         <div class="row">
                             <p class="w-100 subtitleFontFamily">
-                                I am a recent graduate in Computer Science with a foreign languages diploma and some work experience whosedream is to become a full-time software developer.
+                                Learning how to program has always been the goal of my life. It started with videogames, which I always have had an affinity with. Playing has never been enough nonetheless. What were there really behind them?
+                                <br><br>
+                                I took the first steps towards programming learning Dark Basic, a structured form of BASIC.
+                                <br><br>
+                                Because of various vicissitudes, in September 2008, I was not able to apply to a computer science oriented high school, so I chose a foreign language high school. Foreign languages were my new interest: English, French, Spanish and Latin plus German and Japanese. I also was lucky enough to go abroad and participate in various language courses in Europe.
+                                <br><br>
+                                However, after graduating in June 2013, I did not forget my old passion for programming and I applied to the university to obtain a bachelor's degree in Computer Science.
                             </p>
                         </div>
 
                     </div>
-                    
 
-                   
                 </div>
+
+            </div>
+
 		</div>
 
 		<?php require "php/common/jsForBootstrap.php"?>
